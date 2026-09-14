2a9b6809b38ef8b91d805c228d37a8a55d3eeb22
Dalton Felizarda Sitoe <O_TEU_EMAIL_DO_GITHUB>
2026-09-14 16:14:28 +0200

    feat: adicionar aplicativo Android com Capacitor

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.crolina.app',
+  appName: 'Crolina',
+  webDir: 'dist'
+};
+
+export default config;
